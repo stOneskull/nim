8cd55f8babfbfda8965ce9e6e51d2ad5d81dc306
stOneskull <stOne@skull.casa>
2025-10-04 11:37:33 +1000

    nimblina

---
+++ connect.nim
@@ -0,0 +1,25 @@
+type
+  Person = object
+    name: string
+    age: int
+
+proc introduce(p: Person) =
+  echo "Hello, my name is ", p.name, " and I am ", p.age, " years old."
+  if p.age < 18:
+    echo "I am a minor."
+  elif p.age >= 18 and p.age < 65:
+    echo "I am an adult."
+  else:
+    echo "I am a senior citizen."
+
+
+import random
+
+proc age(): int =
+  randomize()
+  return rand(2..123)
+
+let person1 = Person(name: "Alice", age: age())
+introduce(person1)
+let person2 = Person(name: "Bob", age: age())
+introduce(person2)
